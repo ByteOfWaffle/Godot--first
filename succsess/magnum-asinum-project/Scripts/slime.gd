ab25cfa517724e399a92de838b22e765375c66b4
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-10-10 14:13:43 +0200

    Update, finished player movement

--- succsess/magnum-asinum-project/Scripts/slime.gd
+++ succsess/magnum-asinum-project/Scripts/slime.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+const speed = 60 #enemy movement speed.
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var direction = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += 60 * delta #One pixel per second, delta makes this even, even if fps differenciates.
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
 	
+	position.x += direction * speed * delta #One pixel per second, delta makes this even, even if fps differenciates.
