eee73393ed871089511c4e25fa7389e11b637dbd
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-10-14 11:49:37 +0200

    Update

--- succsess/magnum-asinum-project/Scripts/coin.gd
+++ succsess/magnum-asinum-project/Scripts/coin.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 
 @onready var gamemanager: Node = %Gamemanager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
 	gamemanager.add_point()
-	queue_free()
+	animation_player.play("PickUpAnimation")
