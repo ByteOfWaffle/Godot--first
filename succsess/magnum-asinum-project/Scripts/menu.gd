4aec067ed8f95f606f46d99f542f178ca3fabbf8
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-10-21 11:12:40 +0200

    Fixed raycast

--- succsess/magnum-asinum-project/Scripts/menu.gd
+++ succsess/magnum-asinum-project/Scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
 
 
 func _on_options_pressed() -> void:
