e7753fa686f02d8ee896004522cd5e0056c6d14b
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-13 10:42:54 +0100

    Camera left map limit added

--- succsess/magnum-asinum-project/Scripts/transfer.gd
+++ succsess/magnum-asinum-project/Scripts/transfer.gd
@@ -1,14 +1,4 @@
 extends Area2D
 
-@onready var timertrans: Timer = $Timertrans
-
-
 func _on_body_entered(body: Node2D) -> void:
-	Engine.time_scale = 0.5
-	timertrans.start()
-
-
-func _on_timertrans_timeout() -> void:
-	print("Transfered")
-	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://Scenes/train.tscn")
