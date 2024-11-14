4816cf76128cfd1528e77ac0a4b5c4d20c630f18
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-14 12:52:21 +0100

    Made a saving system where data is saved in a JSON file

---
+++ succsess/magnum-asinum-project/Scenes/transfer.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var lvl1status = false
+
+func _on_body_entered(body: Node2D) -> void:
+	lvl1status = true
+	save()
+	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
+
+func save():
+	var save_data = {
+		"lvl1_completed": lvl1status
+		}
+	
+	var json_string = JSON.stringify(save_data) #converts to JSON
+	var json_file = FileAccess.open("res://savegame.json", FileAccess.WRITE)
+	json_file.store_line(json_string)
+	json_file.close()
