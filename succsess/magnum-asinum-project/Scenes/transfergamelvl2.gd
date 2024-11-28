cd1fa6b5a701014e63ae86271f7c6fbac0ef56f4
Jeremy <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-29 00:45:10 +0100

    Changed it so that first door can be used at any point, since it's the first level, and for the second door to be used the first level has to be cleared, right now it just sends the user to the same place, but the function itself works now.

--- succsess/magnum-asinum-project/Scenes/transfergamelvl2.gd
+++ succsess/magnum-asinum-project/Scenes/transfergamelvl2.gd
@@ -1,1 +1,24 @@
 extends Area2D
+
+var lvl1status = false
+
+func _ready(): #Runs when all nodes have been initialized, hence the name ready, makes it so nothing happens before everything is loaded.
+	recover()
+
+
+
+func recover():
+	var jsonFile = FileAccess.open("res://savegame.json", FileAccess.READ)
+	if jsonFile:
+		var jsonString = jsonFile.get_as_text()
+		jsonFile.close()
+		
+		# from json string to dictionary so in other words, from {"lvl1_completed":true} to "lvl1_completed": lvl1status, and since lvl1_completed is true, lvl1status turns true
+		var saveData = JSON.parse_string(jsonString)
+		if saveData.has("lvl1_completed"):
+			lvl1status = saveData.lvl1_completed
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if lvl1status == true:
+		get_tree().change_scene_to_file("res://Scenes/game.tscn")
