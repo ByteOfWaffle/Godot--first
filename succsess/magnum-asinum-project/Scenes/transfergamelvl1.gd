c9a22c02241b0276ffe286a752163d9dc82879dd
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-12-03 13:34:38 +0100

    made doors work like doors, e must be pressed while player is overlapping

--- succsess/magnum-asinum-project/Scenes/transfergamelvl1.gd
+++ succsess/magnum-asinum-project/Scenes/transfergamelvl1.gd
@@ -1,4 +1,15 @@
 extends Area2D
 
+var player_entered = false
+
 func _on_body_entered(body: Node2D) -> void:
+	player_entered = true
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("use") && player_entered == true:
 		get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	player_entered = false
--- succsess/magnum-asinum-project/Scripts/transfertohub.gd
+++ succsess/magnum-asinum-project/Scripts/transfertohub.gd
@@ -1,4 +1,17 @@
 extends Area2D
 
+var player_entered = false
+
+
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
+	player_entered = true
+
+
+
+func _input(event: InputEvent) -> void: #Build in function for inputs
+	if player_entered == true && Input.is_action_just_pressed("use"):
+		get_tree().change_scene_to_file("res://Scenes/hub.tscn")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	player_entered = false
